94487f171c10112994b89169a986044b7c893682
Petteri Kivimäki <dinky_jackson@hotmail.com>
2014-08-17 15:18:18 +0300

    Added delete rights to MySQL loc_ser_admin user.

--- db/mysql/create_db_users.sql
+++ db/mysql/create_db_users.sql
@@ -4,7 +4,7 @@ CREATE USER 'loc_ser'@'localhost' IDENTIFIED BY 'location_service';
 
 GRANT SELECT ON location_service.MY_USER TO 'loc_ser_login'@'localhost';
 GRANT SELECT ON location_service.USER_INFO TO 'loc_ser_login'@'localhost';
-GRANT SELECT, INSERT, UPDATE, LOCK TABLES ON location_service.* TO 'loc_ser_admin'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, LOCK TABLES ON location_service.* TO 'loc_ser_admin'@'localhost';
 GRANT SELECT, LOCK TABLES ON location_service.* TO 'loc_ser'@'localhost';
 GRANT RELOAD ON *.* TO 'loc_ser_login'@'localhost', 'loc_ser_admin'@'localhost', 'loc_ser'@'localhost';
 GRANT INSERT ON location_service.SEARCH_EVENT TO 'loc_ser'@'localhost';
